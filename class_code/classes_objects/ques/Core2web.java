f28d1c155f88e92dda5cf47711b97558befdbaa9
shri2915 <shriramkhade2915@gmail.com>
2023-10-02 21:12:25 +0530

    i added code

---
+++ class_code/classes_objects/ques/Core2web.java
@@ -0,0 +1,16 @@
+public class Core2web{
+        //int var1=10;
+        public static void main(String[]s){
+           //     core2web c2w=new core2web();
+                System.out.println("in core2web");
+                Main.main(s);
+
+        }
+}
+class Main{
+        public static void main(String[]s){
+                System.out.println("in main");
+             //   core2web c2w=new core2web();
+               // System.out.println(++c2w.var1);
+        }
+}
